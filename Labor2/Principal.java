73cf716b7c57b2fe41cdefbf0cf9224b8f094a76
Bidkar <bidkar_2314@hotmail.com>
2013-06-29 19:33:11 -0500

    arreglando

--- Labor2/Principal.java
+++ Labor2/Principal.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 
 public class Principal {
 
@@ -6,63 +8,18 @@ public class Principal {
 	 */
 	public static void main(String arg[])
     {
-    int numDigitos=0;
-    int ndigitos=0;
+    int result;
+    int n1=10;
+    int n2=20;
     
-    numDigitos=8;
-    if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99998; i++ )
-    {
-        ndigitos=contarDigitos(i);
-        if(ndigitos==numDigitos)
-            if (primo(i) == true) 
-                System.out.println(i);    
-    }
+    
+    result= n1 + n2;
+    
+    
+       System.out.println("La suma de 10 + 20 es: " + result);
+   
     }
 	
 	
-	 public static boolean primo(int num)
-     {
-         boolean p = false;
-  
-         if (num < 4) p = true;
-         else
-         {
-         if (num % 2 == 0) p = false; 
-             else
-             {
-                 int contador = 0;  
-                 int i = 1; 
-                 int limite = (num - 1) / 2; 
-                 if  (limite % 2 == 0) limite--;  
-          
-                 while(i <= limite)
-                 {
-                     if (num % i == 0) contador++;
-                         i += 2;
-                         if (contador == 2) i = limite + 1;
-                 }
-
-     if (contador == 1) p = true;
-         } 
-     } 
-  
-     return p;
- }
-
- public static int contarDigitos(int numeroEntero)
- {
-         int divisionEntera = numeroEntero;
-
-         int contador=0;
-
-         while (divisionEntera != 0)
-     {
-         divisionEntera = divisionEntera / 10;
-         contador++;
-     }
- return contador;
- }
 
 }
